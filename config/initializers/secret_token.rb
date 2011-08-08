33e53b966672863e4c4b5781083019ceed38efb6
Mary Ann Brennan <mbrennan@gmail.com>
2011-08-07 18:55:27 -0700

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whenmill::Application.config.secret_token = 'c0bbbfe0241bb9f4ec74dfe7f213c698407e4569547a9d436bd94efd5c7b9db23fad3d6db742a005d5a8b796a60c42ce6cd529f43b5ce5b4b150612f8e30082d'
